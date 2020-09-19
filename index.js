16484d6245b2b898c30b9e91e81e5e3a1bf60811
German Cabarcas <gcc@unironical.com>
2020-09-19 14:07:16 -0500

    Fixed the catalog req string
    Now the request string get the actual whole catalog.
    Modified the sizing of the logo in the header

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 
 const token = process.env.ECWID_PUBTOKEN;
 const storeId = process.env.ECWID_STOREID;
-const reqString = `https://app.ecwid.com/api/v3/${storeId}/products?category=0&token=${token}`
+const reqString = `https://app.ecwid.com/api/v3/${storeId}/products?token=${token}`
 
 console.log(  reqString )
 
 let res = https.get(reqString, (res) => {
-  console.log( Object.keys(res) );
 
   res.setEncoding('utf8');
   let rawData = '';
@@ -16,9 +15,11 @@ let res = https.get(reqString, (res) => {
   res.on('data', (d) => { rawData += d; });
   res.on('end', () => {
     let parsed = JSON.parse(rawData);
+    console.log('Products pulled from the ecwid catalog');
     parsed.items.forEach(x => {
       delete x.url
       if (x.enabled == true) {
+        console.log( x.name );
         fs.writeFileSync(`./content/products/${String(x.name).toLowerCase()}.html`, `${JSON.stringify(x, undefined, ' ')}
     `)
       }
